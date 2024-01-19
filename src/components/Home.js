9ae816f966bfaebf17e4e696d4b61b18c4950601
Tanishq Bedi <tanishq.bedi@gmail.com>
2024-01-19 20:08:41 +0530

    Added dropdown
    Added the functionality in the dropdown to run the python script

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,6 +2,7 @@ import React from 'react'
 import '../compCss/Home.css'
 
 import { Link } from 'react-router-dom';
+import SubjectDropdown from './SubjectDropdown';
 
 
 
@@ -10,6 +11,10 @@ export default function Home() {
   return (
     <div className='homeContainer'>
         <Link to="/login">{console.log("Link")}<button className='newEnrollBtn'>New Entrollment</button></Link>
+
+        <SubjectDropdown/>
+        
+        
     </div>
   )
 }
