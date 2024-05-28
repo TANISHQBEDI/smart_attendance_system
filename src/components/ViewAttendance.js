5febaaaf4f62a4e8baf315dc988bc7609a13d476
Tanishq Bedi <tanishq.bedi@gmail.com>
2024-05-28 20:23:20 +0530

    Update ViewAttendance.js

--- src/components/ViewAttendance.js
+++ src/components/ViewAttendance.js
@@ -18,7 +18,7 @@ const AttendanceList = () => {
     const response = await axios.get('https://e716-144-48-178-203.ngrok-free.app/api/viewattendance',{  //  PORT 8080
         params: { subject },
       });
-      console.log("Frontend records response : ",response)
+      console.log("Frontend records response : ",response.data)
 
       if (Array.isArray(response.data)) {
         setAttendanceData(response.data);
